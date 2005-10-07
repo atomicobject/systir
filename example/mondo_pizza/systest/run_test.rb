4aa10a7eb6b4a7b15c49af8d87d91269e4f09282
karlin <karlin@5e93408f-b7ed-0310-b26a-d648f8c3d419>
2005-10-07 04:13:33 +0000

    added automatic idea->test conversion
    git-svn-id: https://svn.atomicobject.com/svn/systir/trunk@44 5e93408f-b7ed-0310-b26a-d648f8c3d419

--- example/mondo_pizza/systest/run_test.rb
+++ example/mondo_pizza/systest/run_test.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/ruby
+require 'test_file_markup'
+require 'fileutils'
 
 # Manually configure the Ruby library load path to ensure example robustness
 $LOAD_PATH << File.dirname(__FILE__) + "/lib"
@@ -10,6 +12,19 @@ testfile = ARGV[0]
 unless testfile
   puts "Usage: run_test.rb <some.test>"
   exit 1
+end
+
+if File.extname(testfile) == '.idea'
+	puts "Converting idea file to test file..."
+	outfile = testfile.gsub('.idea', '.test')
+  FileUtils.rm outfile
+	converter = TestFileMarkup.new
+	
+	out = converter.convert(testfile)
+	output = File.new(outfile, File::CREAT|File::WRONLY, 0644)
+  output.puts out
+	output.close
+	testfile = outfile
 end
 
 Systir::Launcher.new.run_test PizzaDriver, testfile
