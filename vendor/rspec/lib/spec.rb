ede85fb14af9b9d00bd4f49824f8e780d391abb8
alles <alles@5e93408f-b7ed-0310-b26a-d648f8c3d419>
2006-07-01 17:59:02 +0000

    upgrading rspec
    git-svn-id: https://svn.atomicobject.com/svn/systir/trunk@77 5e93408f-b7ed-0310-b26a-d648f8c3d419

--- vendor/rspec/lib/spec.rb
+++
@@ -1,6 +0,0 @@
-$LOAD_PATH.push File.dirname(__FILE__) + '/../lib'
-
-require 'spec/version'
-require 'spec/api'
-require 'spec/runner'
-require 'spec/runner/kernel_ext'
--- vendor/rspec/lib/spec/api/exceptions.rb
+++
@@ -1,9 +0,0 @@
-module Spec
-  module Api
-    class ExpectationNotMetError < StandardError
-    end
-    
-    class MockExpectationError < StandardError
-    end
-  end
-end
